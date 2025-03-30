91d398c0549c35b0ac0539f7145f27c53dd81bb9
Rabah-Muhammed <rabarabi31@gmail.com>
2025-03-31 00:07:35 +0530

    fix:api.js updated

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,22 +1,21 @@
+// src/utils/api.js
 import axios from "axios";
 
-const isDevelopment = import.meta.env.MODE === "development" || process.env.NODE_ENV === "development";
-
-const myBaseUrl =
-  import.meta.env.VITE_API_BASE_URL_LOCAL && import.meta.env.VITE_API_BASE_URL_DEPLOY
-    ? isDevelopment
-      ? import.meta.env.VITE_API_BASE_URL_LOCAL
-      : import.meta.env.VITE_API_BASE_URL_DEPLOY
-    : "http://localhost:8000/api"; // Fallback in case env vars are missing
+const isDevelopment = import.meta.env.MODE === "development";
+const myBaseUrl = isDevelopment
+  ? import.meta.env.VITE_API_BASE_URL_LOCAL  // "http://localhost:8000/api"
+  : import.meta.env.VITE_API_BASE_URL_DEPLOY; // "https://bookbounty-backend.onrender.com/api"
 
 console.log("API Base URL:", myBaseUrl); // Debugging - check if the correct URL is used
 
 const api = axios.create({
-  baseURL: myBaseUrl,
+  baseURL: myBaseUrl, 
   headers: {
     "Content-Type": "application/json",
   },
   timeout: 10000,
 });
+
+
 
 export default api;
