adfc91895e72dfc34892e26d800630d6425be66d
Rabah-Muhammed <rabarabi31@gmail.com>
2025-03-30 17:13:44 +0530

    fix:api.js file updated

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,16 +1,19 @@
-// src/utils/api.js
-import axios from "axios";
+
+import axios from 'axios';
 
 const isDevelopment = import.meta.env.MODE === 'development'
-const myBaseUrl = isDevelopment ? import.meta.env.VITE_API_BASE_URL_LOCAL : import.meta.env.VITE_API_BASE_URL_DEPLOY
+const BaseUrl = isDevelopment ? import.meta.env.VITE_API_BASE_URL_LOCAL : import.meta.env.VITE_API_BASE_URL_DEPLOY
 
+// Create an instance of Axios and store it in the 'apiInstance' variable. This instance will have specific configuration options.
 const api = axios.create({
-  baseURL: myBaseUrl, 
-  headers: {
-    "Content-Type": "application/json",
-  },
-  timeout: 10000,
-});
+    baseURL: BaseUrl,
+    timeout: 50000,
 
+    // Define headers that will be included in every request made using this instance. This is common for specifying the content type and accepted response type.
+    headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+    },
+});
 
 export default api;
