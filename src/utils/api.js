fabb18011866b06a7045a2ce4818814e0cb7232e
Rabah-Muhammed <rabarabi31@gmail.com>
2025-03-30 17:03:48 +0530

    fix:api.js file updated

--- src/utils/api.js
+++ src/utils/api.js
@@ -12,50 +12,5 @@ const api = axios.create({
   timeout: 10000,
 });
 
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("access_token");
-    if (token) {
-      config.headers["Authorization"] = `Bearer ${token}`;
-     
-    } else {
-      console.log("No access token found in localStorage");
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
-
-api.interceptors.response.use(
-  (response) => response,
-  async (error) => {
-    const originalRequest = error.config;
-    if (error.response?.status === 401 && !originalRequest._retry) {
-      originalRequest._retry = true;
-      try {
-        const refreshToken = localStorage.getItem("refresh_token");
-        if (!refreshToken) throw new Error("No refresh token available");
-
-        console.log("Attempting token refresh...");
-        const response = await axios.post(
-          "http://127.0.0.1:8000/api/token/refresh/", // Hardcoded for consistency
-          { refresh: refreshToken },
-          { headers: { "Content-Type": "application/json" } }
-        );
-        const newAccessToken = response.data.access;
-        localStorage.setItem("access_token", newAccessToken);
-        originalRequest.headers["Authorization"] = `Bearer ${newAccessToken}`;
-        return api(originalRequest);
-      } catch (refreshError) {
-
-        localStorage.removeItem("access_token");
-        localStorage.removeItem("refresh_token");
-        window.location.href = "/login";
-        return Promise.reject(refreshError);
-      }
-    }
-    return Promise.reject(error);
-  }
-);
 
 export default api;
